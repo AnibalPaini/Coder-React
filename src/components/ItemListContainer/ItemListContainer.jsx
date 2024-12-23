02e6983a02968f5bf5afa1d88696e3ce94813594
AnibalPaini <anibalpaini01@gmail.com>
2024-12-23 12:51:57 -0300

    Guardar

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 
 function ItemListContainer(props) {
   return (
-    <main className="container-xl">
-        <h2>Productos</h2>
+    <main className="container-sm mt-5">
+        <h2 className='fontP'>Productos</h2>
         <div className="container">
-            <div className="row ">
+            <div className="row gap-1">
                 <div className="col-sm mb-3 bg-primary text-white text-center rounded p-3"><p>{props.producto}</p></div>
                 <div className="col-sm mb-3 bg-secondary text-white text-center rounded p-3"><p>{props.producto}</p></div>
                 <div className="col-sm mb-3 bg-primary text-white text-center rounded p-3"><p>{props.producto}</p></div>
-            </div>
-            <div className="row">
-                <div className="col-sm mb-3 bg-secondary text-white text-center rounded p-3"><p>{props.producto}</p></div>
-                <div className="col-sm mb-3 bg-primary text-white text-center rounded p-3"><p>{props.producto}</p></div>
-                <div className="col-sm mb-3 bg-secondary text-white text-center rounded p-3"><p>{props.producto}</p></div>
             </div>
         </div>
     </main>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import "../../app.css"
 
 function Navbar() {
   return (
-    <header className='container-fluid bg-secondary d-flex justify-content-evenly align-items-center px-5 py-3'>
-        <h1 className='h1'>Camisetas-MD</h1>
+    <header className='container-fluid d-flex justify-content-between align-items-center px-5 py-3 border-bottom border-secondary'>
+        <h1 className='h1 titulo'>Camisetas MD</h1>
         <nav className='d-flex gap-3 align-items-center'>
             <ul className='nav gap-3'> 
                 <li className='nav-item'><a className="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover" href="#">Inicio</a></li>
